f59e0f7b0198549afb27e0d4945aa3487984c7b4
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-09 21:08:37 -0400

    fixed queryURL

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 var topics = ["Doctor Who", "Battlestar Galactica", "Star Wars", "Torchwood", "Lord of the Rings", "Game of Thrones", "Sherlock", "Warehouse 13", "Star Trek"];
 var key = "TB7eTP6O2J0VXAlUwCLj7T96z55fYByl";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + key + "&limit=10";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" + key + "&limit=10";
 
 $.ajax({
     url: queryURL,
@@ -8,8 +8,11 @@ $.ajax({
   }).then(function(response) {
         console.log(response);
         makeButtons();
+        console.log(response.data[0].images.fixed_height_small);
+        $("#gifsloc").text(JSON.stringify(response.data[0].url));
 });
 
+//create buttons form topics array
 function makeButtons() {
 
     $("#showbuttonloc").empty();
@@ -19,3 +22,12 @@ function makeButtons() {
         $("#showbuttonloc").append(html);
     };
 }
+
+//display gif and info required
+function displaygif() {
+
+}
+
+//push entry to array
+
+
