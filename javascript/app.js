5a734858d6b473f3ec3d2f8ae2cd76be999a647a
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-14 09:59:09 -0400

    working on appending gifs

--- javascript/app.js
+++ javascript/app.js
@@ -8,10 +8,28 @@ $.ajax({
   }).then(function(response) {
         console.log(response);
         makeButtons();
-        console.log(response.data[0].images.fixed_height_small);
+        console.log("url: " + response.data[3].images.fixed_height_small.url);
         
-        var gifURL = response.data[0].url;
-        $("#gifsloc").attr("src", gifURL);
+        
+
+        // Add for each to displaygifs
+        $(".tvbttn").on("click", function() {
+            console.log("you clicked a tv show");
+            var show = $(".tvbttn").val();
+            var gifURL = response.data[3].images.fixed_height_small.url;
+            var p = $("<p>")
+            $(p).attr("src", gifURL);
+            $("#gifsloc").prepend(p); 
+            
+        });
+
+        // $(".tvbttn").on("click", function() {
+        //     for (s=0; s<10; s++) {
+        //         var choice = $(".tvbttn").val();
+        //         var static = response.data[s].images.fixed_height_small.url;
+        //         $("#gifsloc").append(static);
+        //     };
+        // });
         
 });
 
@@ -21,27 +39,33 @@ function makeButtons() {
     $("#showbuttonloc").empty();
 
     for (m=0; m<topics.length; m++) {
-        var html = "<button>" + topics[m] + "</button>";
-        $("#showbuttonloc").append(html);
+        var bttnhtml = $("<button>");
+        bttnhtml.addClass("tvbttn");
+        bttnhtml.text(topics[m]);
+        $("#showbuttonloc").append(bttnhtml);
     };
 }
 
-//display gif and info required
-function displaygif() {
 
+//display gif and info required
+//trying this above without
+function displaygif() {
+    console.log("displaygif function ran");
+    var show = $(".tvbttn").val();
+
+    $("#gifsloc").append();
 }
 
 //push entry to array
 $("#searchbutton").on("click", function() {
     event.preventDefault();
-    console.log("click function ran");
+    // console.log("click function ran");
     var enteredshow = $("#gifsearch").val()
     topics.push(enteredshow);
-    console.log(topics);
+    // console.log(topics);
 
-    //FIX ERROR FOR BELOW LINE 
+    //clear form after entry has been added to array
     $("#gifsearch").val("");
-    console.log("value: " + $("#gifsearch").val())
     makeButtons();
 });
 
